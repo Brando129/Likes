90b546733b591740bf578be90fcf5bbb2aa662e6
Brandon Colangelo <brandoncancode@gmail.com>
2023-03-25 16:35:14 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var postLikes1 = 3;
+var cardLikes1 = document.querySelector("#cardLikes1");
+
+
+function thumbsUp1() {
+    postLikes1++;
+    cardLikes1.innerText = postLikes1 + " like(s)";
+}
+
+var postLikes2 = 12;
+var cardLikes2 = document.querySelector("#cardLikes2");
+
+
+function thumbsUp2() {
+    postLikes2++;
+    cardLikes2.innerText = postLikes2 + " like(s)";
+}
+
+var postLikes3 = 9;
+var cardLikes3 = document.querySelector("#cardLikes3");
+
+
+function thumbsUp3() {
+    postLikes3++;
+    cardLikes3.innerText = postLikes3 + " like(s)";
+}
